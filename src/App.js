1f058af313bac913d22f02c2118442cf45e5225e
Lance <lancehahn@gmail.com>
2022-10-06 00:26:20 +0900

    add the mint functin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import { ethers } from "ethers";
+import myEpicNft from "./utils/MyEpicNFT.json"
 
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
@@ -52,9 +54,52 @@ const App = () => {
       * Boom! This should print out public address once we authorize Metamask.
       */
       console.log("Connected", accounts[0]);
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+      const goerliChainId = "0x5"; 
+      if (chainId == goerliChainId) {
+        alert("You are connected to the Goerli Test Network!");
+      } else {
+        alert("You are not connected to the Goerli Test Network!");
+      }      
       setCurrentAccount(accounts[0]); 
     } catch (error) {
       console.log(error);
+    }
+  }
+
+
+  const askContractToMintNft = async () => {
+    //It should be store in the ENV in the real env
+    const CONTRACT_ADDRESS = "0xcef0fe54ef8b6056750d8711cbfdcf92c35dc159";
+  
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+  
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+  
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+        
+        console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+        
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: <https://testnets.opensea.io/assets/goerli/${CONTRACT_ADDRESS}/${tokenId.toNumber()}>`)
+        });
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -65,6 +110,7 @@ const App = () => {
     </button>
   );
 
+  
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -83,9 +129,9 @@ const App = () => {
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={null} className="cta-button connect-wallet-button">
-              Mint NFT
-            </button>
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+            Mint NFT
+          </button>
           )}
         </div>
         <div className="footer-container">
